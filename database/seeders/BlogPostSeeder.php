6c5e27ae0d4daa0c383c55805177df32b75e576b
Nolwen Major Francès <115166022+NolwenMaj@users.noreply.github.com>
2023-04-12 17:04:27 +0200

    tentative show blogposts

---
+++ database/seeders/BlogPostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BlogPostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\BlogPost::factory(10)->create();
+
+        \App\Models\BlogPost::factory()->create([
+            'title' => 'Test title',
+            'content' => 'Test content',
+            'image' => 'https://test.com',
+        ]);
+    }
+}

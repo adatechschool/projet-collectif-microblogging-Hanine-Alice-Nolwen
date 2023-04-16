f5e465bf94d3ad9a8c3a48f55f0bc491d3bfd294
“Hanineali” <hanineali.bc@gmail.com>
2023-04-16 22:45:22 +0200

    Adding views

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="text-gray-700 body-font overflow-hidden bg-white">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <img alt="blogpost_img" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="{{$blogpost->image}}">
+            <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h2 class="text-sm title-font text-gray-500 tracking-widest">Title</h2>
+                <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$blogpost->title}}</h1>
+                <p class="leading-relaxed">{{$blogpost->content}}</p>
+                <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+
+                </div>
+                <div class="flex">
+                    <span class="title-font font-medium text-2xl text-gray-900">User: {{$blogpost->user_id}} - </span>
+                    <span class="title-font font-medium text-2xl text-gray-900"> {{$blogpost->updated_at}}</span>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection

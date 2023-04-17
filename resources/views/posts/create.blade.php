000f8fc1fe9bfdddcc6b0f4c51e55c6964fdfd67
“Hanineali” <hanineali.bc@gmail.com>
2023-04-17 14:56:06 +0200

    create_post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div>
+    <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+        <div class="w-full px-16 py-20 mt-6 overflow-hidden bg-white rounded-lg lg:max-w-4xl">
+            <div class="mb-4">
+                <h1 class="font-serif text-3xl font-bold decoration-gray-400">
+                    Create New Post
+                </h1>
+            </div>
+
+            <div class="w-full px-6 py-4 bg-white rounded shadow-md ring-1 ring-gray-900/10">
+                <form method="POST" action="">
+                    @csrf
+                    <!-- Title -->
+                    <div>
+                        <label class="block text-sm font-bold text-gray-700" for="title">
+                            Title
+                        </label>
+
+                        <input class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="text" name="title" id="title" placeholder="Enter A Post Title" />
+                    </div>
+
+                    <!-- Content -->
+                    <div class="mt-4">
+                        <label class="block text-sm font-bold text-gray-700" for="content">
+                            Content
+                        </label>
+                        <textarea name="content" id="content" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="" placeholder="Write Your Post" required></textarea>
+                    </div>
+
+
+                    <div class="flex items-center justify-center w-full pt-10">
+                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+                                </svg>
+                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                            </div>
+                            <input id="dropzone-file" type="file" class="hidden" />
+                        </label>
+                    </div>
+
+
+                    <div class="flex items-center justify-start mt-4 gap-x-2">
+                        <button type="submit" class="px-6 py-2 text-sm font-semibold rounded-md shadow-md text-sky-100 bg-sky-500 hover:bg-sky-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300">
+                            Save
+                        </button>
+                        <button type="submit" class="px-6 py-2 text-sm font-semibold text-gray-100 bg-gray-400 rounded-md shadow-md hover:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300">
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
